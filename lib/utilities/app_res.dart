456d3e9e72a024913d2edee603c658c5d2252a14
king-hub259 <84419911+king-hub259@users.noreply.github.com>
2026-04-20 12:29:08 +0100

    Rename app from 'Shortzz' to 'Palaver'

--- lib/utilities/app_res.dart
+++ lib/utilities/app_res.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 import 'package:shortzz/languages/languages_keys.dart';
 
 class AppRes {
-  static String appName = 'Shortzz';
+  static String appName = 'Palaver';
 
   static String gifBrandName = 'GIPHY';
 
